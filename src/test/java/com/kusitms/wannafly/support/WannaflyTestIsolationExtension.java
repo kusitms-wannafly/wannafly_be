ac50e47b4533fc2607de4f16800c580405da79c0
does <ldk980130@gmail.com>
2023-04-15 17:47:05 +0900

    test: 테스트 격리 Extension 구현

---
+++ src/test/java/com/kusitms/wannafly/support/WannaflyTestIsolationExtension.java
@@ -0,0 +1,19 @@
+package com.kusitms.wannafly.support;
+
+import org.junit.jupiter.api.extension.AfterEachCallback;
+import org.junit.jupiter.api.extension.ExtensionContext;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+public class WannaflyTestIsolationExtension implements AfterEachCallback  {
+
+    @Override
+    public void afterEach(ExtensionContext context) throws Exception {
+        WannaflyDatabaseManager databaseManager = getWannaflyDatabaseManager(context);
+        databaseManager.truncateTables();
+    }
+
+    private WannaflyDatabaseManager getWannaflyDatabaseManager(ExtensionContext context) {
+        return (WannaflyDatabaseManager) SpringExtension
+                .getApplicationContext(context).getBean("wannaflyDatabaseManager");
+    }
+}
