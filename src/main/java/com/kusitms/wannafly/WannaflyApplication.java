a83e0fcf18d73c243ea6680bb2bd4c607f197a9c
does <ldk980130@gmail.com>
2023-04-15 17:20:09 +0900

    chore: 프로젝트 초기 세팅

---
+++ src/main/java/com/kusitms/wannafly/WannaflyApplication.java
@@ -0,0 +1,13 @@
+package com.kusitms.wannafly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WannaflyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WannaflyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kusitms/wannafly/WannaflyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kusitms.wannafly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WannaflyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
