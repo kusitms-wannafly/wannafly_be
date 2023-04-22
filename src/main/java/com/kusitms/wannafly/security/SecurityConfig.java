7ed05c7a621b825612ca3845165f4799515ffb02
does <ldk980130@gmail.com>
2023-04-22 15:51:47 +0900

    chore: 구글 로그인 및 시큐리티 설정

---
+++ src/main/java/com/kusitms/wannafly/security/SecurityConfig.java
@@ -0,0 +1,29 @@
+package com.kusitms.wannafly.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+
+                .formLogin().disable()
+                .httpBasic().disable()
+
+                .oauth2Login()
+                .and()
+
+                .build();
+    }
+}
