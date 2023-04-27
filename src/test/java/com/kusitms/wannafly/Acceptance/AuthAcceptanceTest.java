d0646c823f7679fb51681f439c7e418f1443398a
does <ldk980130@gmail.com>
2023-04-27 22:28:54 +0900

    test: 이메일 중복 인수 테스트 작성

--- src/test/java/com/kusitms/wannafly/Acceptance/AuthAcceptanceTest.java
+++ src/test/java/com/kusitms/wannafly/Acceptance/AuthAcceptanceTest.java
@@ -16,6 +16,13 @@ class AuthAcceptanceTest extends AcceptanceTest {
     @DisplayName("로그인을 하면 회원 식별자와 엑세스 토큰, 리프레시 토큰을 응답한다.")
     @Test
     void oauthLogin() {
+        // given
+        RestAssured.given().log().all()
+                .when()
+                .get("/mock/oauth2/authorization/google")
+                .then().log().all()
+                .extract();
+
         // when
         ExtractableResponse<Response> response = RestAssured.given().log().all()
                 .when()
@@ -30,6 +37,30 @@ class AuthAcceptanceTest extends AcceptanceTest {
 
                 () -> assertThat(actual.memberId()).isNotNull(),
                 () -> assertThat(actual.accessToken()).isNotNull()
+        );
+    }
+
+    @DisplayName("다른 OAuth Client, 같은 이메일로 가입을 시도하면 400 예외가 발생한다.")
+    @Test
+    void oauthLoginDuplicatedEmail() {
+        // given
+        RestAssured.given().log().all()
+                .when()
+                .get("/mock/oauth2/authorization/google")
+                .then().log().all()
+                .extract();
+
+        // when
+        ExtractableResponse<Response> response = RestAssured.given().log().all()
+                .when()
+                .get("/mock/oauth2/authorization/naver")
+                .then().log().all()
+                .extract();
+
+        // then
+        assertAll(
+                () -> assertThat(response.statusCode()).isEqualTo(HttpStatus.BAD_REQUEST.value()),
+                () -> assertThat(response.jsonPath().getInt("errorCode")).isEqualTo(1001)
         );
     }
 }
