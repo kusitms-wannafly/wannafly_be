3df7119209f8781c058aff892e97d2e36cfedf94
does <ldk980130@gmail.com>
2023-05-07 23:03:23 +0900

    refactor: OAuthProvider로 if문 리팩터링

--- src/main/java/com/kusitms/wannafly/auth/security/authentication/PrincipalOAuth2UserService.java
+++ src/main/java/com/kusitms/wannafly/auth/security/authentication/PrincipalOAuth2UserService.java
@@ -4,20 +4,16 @@ import com.kusitms.wannafly.auth.application.AuthService;
 import com.kusitms.wannafly.auth.dto.LoginRequest;
 import com.kusitms.wannafly.auth.dto.LoginResponse;
 import com.kusitms.wannafly.auth.security.oauth.OAuth2Member;
-import com.kusitms.wannafly.auth.security.oauth.RegistrationId;
+import com.kusitms.wannafly.auth.security.oauth.OAuthProvider;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
 
-import java.util.Map;
-
 @Service
 @RequiredArgsConstructor
-@Slf4j
 public class PrincipalOAuth2UserService extends DefaultOAuth2UserService {
 
     private final AuthService authService;
@@ -35,53 +31,7 @@ public class PrincipalOAuth2UserService extends DefaultOAuth2UserService {
 
     private LoginRequest toLoginRequest(OAuth2UserRequest userRequest) {
         OAuth2User oAuth2User = super.loadUser(userRequest);
-        log.info("#########oAuth2User.toString()");
-
-        RegistrationId registrationId = RegistrationId.from(
-                userRequest.getClientRegistration().getRegistrationId()
-        );
-
-        if (registrationId.getValue().equals("naver")) {
-            Map<String, String> response = (Map<String, String>) oAuth2User.getAttribute("response");
-            log.info(response.toString());
-            LoginRequest loginRequest = new LoginRequest(
-                    registrationId.getValue(),
-                    response.get(registrationId.getNameAttribute()),
-                    response.get(registrationId.getEmailAttribute()),
-                    response.get(registrationId.getPictureUrlAttribute())
-            );
-            log.info(loginRequest.toString());
-            return loginRequest;
-        }
-        else if (registrationId.getValue().equals("kakao")) {
-            Map<String, Object> attributes = oAuth2User.getAttributes();
-            log.info(attributes.toString());
-            Map<String, Object> kakao_account = (Map<String, Object>) attributes.get("kakao_account");
-            String email = (String) kakao_account.get("email");
-            Map<String, Object> properties = (Map<String, Object>) attributes.get("properties");
-            String nickname = (String) properties.get("nickname");
-            String url = "null";
-            LoginRequest loginRequest = new LoginRequest(
-                    registrationId.getValue(),
-                    email,
-                    nickname,
-                    url
-            );
-            log.info(loginRequest.toString());
-            return loginRequest;
-        }
-        LoginRequest loginRequest = new LoginRequest(
-                registrationId.getValue(),
-                oAuth2User.getAttribute(registrationId.getNameAttribute()),
-                oAuth2User.getAttribute(registrationId.getEmailAttribute()),
-                oAuth2User.getAttribute(registrationId.getPictureUrlAttribute())
-        );
-        log.info(loginRequest.toString());
-        return new LoginRequest(
-                registrationId.getValue(),
-                oAuth2User.getAttribute(registrationId.getNameAttribute()),
-                oAuth2User.getAttribute(registrationId.getEmailAttribute()),
-                oAuth2User.getAttribute(registrationId.getPictureUrlAttribute())
-        );
+        OAuthProvider oAuthProvider = OAuthProvider.from(userRequest.getClientRegistration().getRegistrationId());
+        return oAuthProvider.toLoginRequest(oAuth2User);
     }
 }
---
+++ src/main/java/com/kusitms/wannafly/auth/security/oauth/OAuthProvider.java
@@ -0,0 +1,55 @@
+package com.kusitms.wannafly.auth.security.oauth;
+
+import com.kusitms.wannafly.auth.dto.LoginRequest;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+
+import java.util.Map;
+
+public enum OAuthProvider {
+
+    GOOGLE {
+        @Override
+        public LoginRequest toLoginRequest(OAuth2User oAuth2User) {
+            return new LoginRequest(
+                    name().toLowerCase(),
+                    oAuth2User.getAttribute("name"),
+                    oAuth2User.getAttribute("email"),
+                    oAuth2User.getAttribute("picture")
+            );
+        }
+    },
+    KAKAO {
+        @Override
+        public LoginRequest toLoginRequest(OAuth2User oAuth2User) {
+            Map<String, Object> attributes = oAuth2User.getAttributes();
+            Map<String, Object> kakao_account = (Map<String, Object>) attributes.get("kakao_account");
+            Map<String, Object> properties = (Map<String, Object>) attributes.get("properties");
+            return new LoginRequest(
+                    name().toLowerCase(),
+                    (String) kakao_account.get("email"),
+                    (String) properties.get("nickname"),
+                    "null"
+            );
+        }
+    },
+    NAVER {
+        @Override
+        public LoginRequest toLoginRequest(OAuth2User oAuth2User) {
+            Map<String, String> response = oAuth2User.getAttribute("response");
+            return new LoginRequest(
+                    name().toLowerCase(),
+                    response.get("name"),
+                    response.get("email"),
+                    response.get("profile_image")
+            );
+        }
+    }
+
+    ;
+
+    public abstract LoginRequest toLoginRequest(OAuth2User oAuth2User);
+
+    public static OAuthProvider from(String registrationId) {
+        return valueOf(registrationId.toUpperCase());
+    }
+}
