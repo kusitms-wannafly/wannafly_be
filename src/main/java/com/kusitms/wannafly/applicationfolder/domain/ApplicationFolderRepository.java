464309322560c455771868458eb106034d7440ff
0sik <ghdtlrdudtlr@naver.com>
2023-05-11 12:46:06 +0900

    feat : 같은 년도일때 errorcode 추가

--- src/main/java/com/kusitms/wannafly/applicationfolder/domain/ApplicationFolderRepository.java
+++ src/main/java/com/kusitms/wannafly/applicationfolder/domain/ApplicationFolderRepository.java
@@ -1,6 +1,15 @@
 package com.kusitms.wannafly.applicationfolder.domain;
 
+import com.kusitms.wannafly.auth.LoginMember;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ApplicationFolderRepository extends JpaRepository<ApplicationFolder,Long> {
+
+    static boolean existsByMemberIdAndYear(long memberId, int year) {
+        return true;
+    }
+
+    static Boolean existsByYear(LoginMember loginMember, Integer year) {
+        return existsByMemberIdAndYear(loginMember.id(), year);
+    }
 }
--- src/main/java/com/kusitms/wannafly/applicationfolder/presentation/ApplicationFolderController.java
+++ src/main/java/com/kusitms/wannafly/applicationfolder/presentation/ApplicationFolderController.java
@@ -1,8 +1,12 @@
 package com.kusitms.wannafly.applicationfolder.presentation;
 
+import com.kusitms.wannafly.applicationfolder.domain.ApplicationFolderRepository;
 import com.kusitms.wannafly.applicationfolder.dto.ApplicationFolderCreateRequest;
 import com.kusitms.wannafly.applicationfolder.service.ApplicationFolderService;
 import com.kusitms.wannafly.auth.LoginMember;
+import com.kusitms.wannafly.exception.BusinessException;
+import com.kusitms.wannafly.exception.ErrorCode;
+import com.kusitms.wannafly.member.domain.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -20,6 +24,9 @@ public class ApplicationFolderController {
     @PostMapping
     public ResponseEntity<Void> createFolder(@RequestBody ApplicationFolderCreateRequest request,
                                              LoginMember loginMember){
+        if (ApplicationFolderRepository.existsByYear(loginMember,request.year())){
+            throw BusinessException.from(ErrorCode.MEMBER_DUPLICATE_YEAR);
+        }
         Long folderId = applicationFolderService.createFolder(loginMember,request);
         return ResponseEntity.created(URI.create("/application-folder/"+folderId))
                 .build();
