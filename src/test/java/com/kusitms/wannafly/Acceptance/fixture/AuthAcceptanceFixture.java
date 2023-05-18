83859cfe5c8aaa37f3fe212d7ec39951f5d05f84
0sik <ghdtlrdudtlr@naver.com>
2023-05-18 19:18:09 +0900

    refact : 지원서 보관함 파일 구조 변경

--- src/test/java/com/kusitms/wannafly/Acceptance/fixture/AuthAcceptanceFixture.java
+++ src/test/java/com/kusitms/wannafly/Acceptance/fixture/AuthAcceptanceFixture.java
@@ -1,11 +1,8 @@
 package com.kusitms.wannafly.Acceptance.fixture;
 
-import com.kusitms.wannafly.command.applicationfolder.dto.ApplicationFolderCreateRequest;
 import io.restassured.RestAssured;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
-import org.apache.http.HttpHeaders;
-import org.springframework.http.MediaType;
 
 public class AuthAcceptanceFixture {
 
@@ -34,24 +31,6 @@ public class AuthAcceptanceFixture {
                 .then().log().all()
                 .extract();
     }
-    public static ExtractableResponse<Response> 지원서_보관함을_생성한다(String accessToken, ApplicationFolderCreateRequest request){
-        return RestAssured.given().log().all()
-                .when()
-                .header(HttpHeaders.AUTHORIZATION, "Bearer " + accessToken)
-                .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .body(request)
-                .post("/api/application-folders")
-                .then().log().all()
-                .extract();
-    }
-    public static ExtractableResponse<Response> 지원서를_조회한다(String accessToken){
-        return RestAssured.given().log().all()
-                .when()
-                .header(HttpHeaders.AUTHORIZATION, "Bearer " + accessToken)
-                .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .get("/api/application-folders")
-                .then().log().all()
-                .extract();
-    }
+
 }
 
