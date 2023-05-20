f5cad0ab7713bbe14da032091148530eed235aeb
does <ldk980130@gmail.com>
2023-05-20 17:03:19 +0900

    fix: 인덱스 설정 수정

--- src/main/resources/db/migration/V5__add_category_id_item.sql
+++ src/main/resources/db/migration/V5__add_category_id_item.sql
@@ -1,3 +1,3 @@
 ALTER TABLE application_item ADD category_id BIGINT;
 
-CREATE INDEX idx_category_of_item ON category (category_id);
+CREATE INDEX idx_category_of_item ON application_item (category_id);
