b719407091af8bfe7cee8791457c5740d3f81f51
0sik <ghdtlrdudtlr@naver.com>
2023-05-23 20:47:30 +0900

    fix : redis삭제

--- src/main/java/com/kusitms/wannafly/command/auth/infrastructure/refreshtoken/JpaRefreshTokenRepository.java
+++ src/main/java/com/kusitms/wannafly/command/auth/infrastructure/refreshtoken/JpaRefreshTokenRepository.java
@@ -3,4 +3,5 @@ package com.kusitms.wannafly.command.auth.infrastructure.refreshtoken;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface JpaRefreshTokenRepository extends JpaRepository<JpaRefreshToken, String> {
+
 }
