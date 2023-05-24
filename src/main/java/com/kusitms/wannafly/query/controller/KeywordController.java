875b1168541be3d97390f2d3c807a8df281ef4bd
0sik <ghdtlrdudtlr@naver.com>
2023-05-25 02:47:13 +0900

    fix : url수정

--- src/main/java/com/kusitms/wannafly/query/controller/KeywordController.java
+++ src/main/java/com/kusitms/wannafly/query/controller/KeywordController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("api")
+@RequestMapping("/api")
 @RequiredArgsConstructor
 public class KeywordController {
     private final KeywordService keywordService;
