104436698110abc93e2983255cd0f7d64a4a0efa
0sik <ghdtlrdudtlr@naver.com>
2023-05-25 15:02:24 +0900

    test : redis 볼륨 설정 테스트

--- src/main/java/com/kusitms/wannafly/command/auth/infrastructure/refreshtoken/redis/RedisConfig.java
+++ src/main/java/com/kusitms/wannafly/command/auth/infrastructure/refreshtoken/redis/RedisConfig.java
@@ -12,6 +12,7 @@ import org.springframework.data.redis.repository.configuration.EnableRedisReposi
 @EnableRedisRepositories
 public class RedisConfig {
 
+
     @Value("${spring.redis.host}")
     private String host;
 
